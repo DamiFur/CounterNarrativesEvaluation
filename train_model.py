c2d4396bcb780e494e92e30ab3297dc21bce2b55
DamiFur <damian.a.furman@gmail.com>
2023-08-23 13:03:57 -0300

    k folds and arguments for script

--- train_model.py
+++ train_model.py
@@ -14,26 +14,29 @@ from transformers import EvalPrediction
 from sklearn import metrics
 from transformers import EarlyStoppingCallback
 
+import argparse
 
+device = torch.device("cuda")
+parser = argparse.ArgumentParser(description="Train models for automatic evaluation of counter-narratives")
+parser.add_argument('--lr', type=float, default=2e-05, help="Learning rate for training the model. Default value is 2e-05")
+parser.add_argument("--model_name", type=str, default="google/flan-t5-base")
+parser.add_argument("--category", type=str, default="offensive")
+parser.add_argument("--language", type=str, default="english")
 
-LEARNING_RATE = 2e-05
-NUMBER_OF_PARTITIONS = 10
+args = parser.parse_args()
+
+LEARNING_RATE = args.lr
 device = torch.device("cpu")
 BATCH_SIZE = 4
-EPOCHS = 20 * (BATCH_SIZE / 16)
-MODEL_NAME = "roberta-base"
-TARGET = "stance"
-LANGUAGE = "english"
-# REP=0
-# FOLDS=3
+EPOCHS = 40 * (BATCH_SIZE / 16)
+MODEL_NAME = args.model_name
+TARGET = args.category
+LANGUAGE = args.language
+K_FOLDS = 3
 SEQ_LENGTH = 127
 
 col_names = ["tweet", "cn", "offensive", "stance", "informativeness", "felicity"]
 data = pd.read_csv("datasets/cn_dataset_{}.csv".format(LANGUAGE), names=col_names)
-
-train_val, test = train_test_split(data, test_size=0.1)
-
-train, dev = train_test_split(train_val, test_size=0.05)
 
 model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, num_labels=3)
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, add_prefix_space=True)
@@ -45,11 +48,6 @@ def tokenize_example(example):
     tokenized_input["labels"] = example[TARGET] -1
     return tokenized_input
 
-training_set = Dataset.from_pandas(train).map(tokenize_example)
-
-dev_set = Dataset.from_pandas(dev).map(tokenize_example)
-
-test_set = Dataset.from_pandas(test).map(tokenize_example)
 
 def compute_metrics_f1(p: EvalPrediction):
     preds = p.predictions.argmax(-1)
@@ -101,25 +99,43 @@ training_args = TrainingArguments(
         load_best_model_at_end=True
     )
 
-trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=training_set,
-        eval_dataset=dev_set,
-        tokenizer=tokenizer,
-        data_collator=data_collator,
-        compute_metrics= compute_metrics_f1,
-        callbacks = [EarlyStoppingCallback(early_stopping_patience=10)]
-    ) 
+def train(training_set, dev_set, test_set, k):
+    trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=training_set,
+            eval_dataset=dev_set,
+            tokenizer=tokenizer,
+            data_collator=data_collator,
+            compute_metrics= compute_metrics_f1,
+            callbacks = [EarlyStoppingCallback(early_stopping_patience=10)]
+        ) 
 
-trainer.train()
+    trainer.train()
 
-results = trainer.predict(test_set)
+    results = trainer.predict(test_set)
 
-filename = "./results_test_{}_{}_{}_{}".format(LEARNING_RATE, MODEL_NAME, TARGET, LANGUAGE)
+    filename = "./results_test_{}_{}_{}_{}_{}".format(k, LEARNING_RATE, MODEL_NAME, TARGET, LANGUAGE)
 
-writer = open(filename, "w")
-writer.write("{},{},{},{}\n".format(results.metrics["test_accuracy"], results.metrics["test_f1"], results.metrics["test_precision"], results.metrics["test_recall"]))
-writer.write("{}\n".format(results.metrics["test_f1_per_category"]))
-writer.write("{}\n".format(results.metrics["test_confusion_matrix"]))
-writer.close()
+    writer = open(filename, "w")
+    writer.write("{},{},{},{}\n".format(results.metrics["test_accuracy"], results.metrics["test_f1"], results.metrics["test_precision"], results.metrics["test_recall"]))
+    writer.write("{}\n".format(results.metrics["test_f1_per_category"]))
+    writer.write("{}\n".format(results.metrics["test_confusion_matrix"]))
+    writer.close()
+
+for k in range(K_FOLDS):
+
+    train_val, test_set = train_test_split(data, test_size=0.1, random_state=42+k)
+
+    train_set, dev_set = train_test_split(train_val, test_size=0.05, random_state=42+k)
+
+    training_set_pd = Dataset.from_pandas(train_set).map(tokenize_example)
+    training_set_pd = training_set_pd[training_set_pd[TARGET] > 0]
+
+    dev_set_pd = Dataset.from_pandas(dev_set).map(tokenize_example)
+    dev_set_pd = dev_set_pd[dev_set_pd[TARGET] > 0]
+
+    test_set_pd = Dataset.from_pandas(test_set).map(tokenize_example)
+    test_set_pd = test_set_pd[test_set_pd[TARGET] > 0]
+
+    train(training_set_pd, dev_set_pd, test_set_pd)
